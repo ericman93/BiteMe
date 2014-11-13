48467d8732376e1c7126a8dd4308209ce138a87c
Eric <ericfeldman93@gmail.com>
2014-11-13 18:04:50 +0200

    Work on map and start filter bar

--- Foddies/app/js/services/meetupsService.js
+++ Foddies/app/js/services/meetupsService.js
@@ -2,7 +2,20 @@
     var meetupService = {}
 
     meetupService.getMeetups = function () {
+        var deferred = $q.defer();
 
+        var requests = [
+            {
+                Name: "Test",
+                Location: {
+                    Latitude: 31.850033,
+                    Longitude: 34.6500523
+                }
+            }
+        ]
+        deferred.resolve(requests)
+
+        return deferred.promise;
     }
 
     return meetupService;
