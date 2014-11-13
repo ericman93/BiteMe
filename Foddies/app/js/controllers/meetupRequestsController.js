0e87279e5392660c9c565235cf2a5c70d148f0bf
= <lian@mail.com>
2014-11-14 01:26:00 +0200

    lian html

---
+++ Foddies/app/js/controllers/meetupRequestsController.js
@@ -0,0 +1,20 @@
+﻿foodiesApp.controller('MeetupRequestsController', ['Meetups', '$scope', '$modal',
+  function (Meetups, $scope, $modal) {
+
+      $scope.requests = Meetups.getMeetups().then(function (data) {
+          console.log(data)
+          $scope.requetsts = data;
+      }, function (error) {
+          $scope.requetsts = []
+          alert(error)
+      });
+
+      $scope.close = function () {
+          $modalInstance.dismiss('cancel');
+
+      $scope.setAccepted = function (request, accepted) {
+          request.Accepted = accepted;
+      }
+  }
+
+]);
