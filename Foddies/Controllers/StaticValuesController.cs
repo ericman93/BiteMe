d709dd032d5aa97646815ade5cad8c160b3fc0b9
Eric <ericfeldman93@gmail.com>
2014-11-14 03:50:59 +0200

    Add statusValueController

---
+++ Foddies/Controllers/StaticValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Foddies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Foddies.Controllers
+{
+    public class StaticValuesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get([FromUri] string listName)
+        {
+            if(listName.ToUpper() == "FOODTYPE")
+            {
+                return FoodTypes.FoodTypeList;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+    }
+}
