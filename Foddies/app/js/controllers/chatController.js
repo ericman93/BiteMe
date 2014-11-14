11090557facbea97103f847f92158bfce7739059
ZivB <zivbensh2@gmail.com>
2014-11-14 04:33:09 +0200

    added chatController.js

---
+++ Foddies/app/js/controllers/chatController.js
@@ -0,0 +1,23 @@
+﻿foodiesApp.controller('ChatController', ['Auth', '$scope',
+  function (Auth, $scope) {
+
+      $scope.messages = "";
+
+      var chatHub = $.connection.userRequestChatHub;
+
+      chatHub.client.onReceivedMessage = function (message) {
+          $scope.messages += message;
+          $scope.messages += '\n';
+      };
+
+      $.connection.hub.start().done(function () {
+          chatHub.server.connect($scope.fromUserId);
+
+          console.log('Client connected to chat server!');
+      });
+
+      $scope.sendNewMessage = function (message) {
+          chatHub.server.sendMessage($scope.toUserId, $scope.newMessage);
+      }
+  }
+]);
