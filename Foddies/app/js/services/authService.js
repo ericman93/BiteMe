91b7e14ad1bdcb4cbb0b16f9c0eafc0069f06eef
Eric <ericfeldman93@gmail.com>
2014-11-14 04:57:46 +0200

    Add tabs at meetup view Show meeups i've requests

--- Foddies/app/js/services/authService.js
+++ Foddies/app/js/services/authService.js
@@ -1,28 +1,32 @@
 ﻿foodiesApp.factory('Auth', ['$http', '$q', function ($http, $q) {
-    var authServies = {}
-
-    authServies.login = function (email, password) {
-
-        $http.post('api/login', { id: undefined, email: email, name: undefined, password: password }).
-            success(function (data, status, headers, config) {
-            }).
-            error(function (data, status, headers, config) {
-            });
-    authServies.login = function (email, password) {
-
-        $http.post('api/login', { id: undefined, email: email, name: undefined, password: password }).
-            success(function (data, status, headers, config) {
-                
-                authServies.authId = data;
-
-            }).
-            error(function (data, status, headers, config) {
-            });
+    var authServies = {
+        userId: 2
     }
+
+    authServies.login = function (email, password) {
+
+        $http.post('api/login', { id: undefined, email: email, name: undefined, password: password }).
+            success(function (data, status, headers, config) {
+            }).
+            error(function (data, status, headers, config) {
+            });
+        authServies.login = function (email, password) {
+
+            $http.post('api/login', { id: undefined, email: email, name: undefined, password: password }).
+                success(function (data, status, headers, config) {
+
+                    authServies.userId = data;
+
+                }).
+                error(function (data, status, headers, config) {
+                });
+        }
+    }
+
 
     authServies.logout = function () {
 
-        
+
     }
 
     return authServies;
