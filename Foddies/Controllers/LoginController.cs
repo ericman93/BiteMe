88a1449f33ade05897dc715280bebc248311354e
Eric <ericfeldman93@gmail.com>
2014-11-14 04:28:05 +0200

    Add missing files Add location search

---
+++ Foddies/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Foddies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Foddies.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public bool Post([FromBody]User user)
+        {
+            return true;
+        }
+    }
+}
